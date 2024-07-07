8d642c1147843cc25ca79ca6dffb7bdcb226faa7
Ahmed Eid <ahmedeidworker@outlook.com>
2024-07-08 01:45:37 +0300

    Add some features to the website

--- footer/footer.js
+++ footer/footer.js
@@ -3,28 +3,5 @@ document.addEventListener("DOMContentLoaded", function () {
     .then((response) => response.text())
     .then((data) => {
       document.getElementById("footer-placeholder").innerHTML = data;
-
-      addFooterFunctionality();
     });
 });
-
-// function addFooterFunctionality() {
-//   const termsSection = document.getElementById("terms_content");
-//   const closeTerms = document.getElementById("close-terms");
-//   const backdrop = document.querySelector(".terms .backdrop");
-//   const termsLink = document.querySelector(".terms-link");
-
-//   termsLink.addEventListener("click", function (event) {
-//     event.preventDefault();
-//     showTerms();
-//   });
-
-//   closeTerms.addEventListener("click", hideTerms);
-//   backdrop.addEventListener("click", hideTerms);
-//   function showTerms() {
-//     termsSection.style.display = "block";
-//   }
-//   function hideTerms() {
-//     termsSection.style.display = "none";
-//   }
-// }
